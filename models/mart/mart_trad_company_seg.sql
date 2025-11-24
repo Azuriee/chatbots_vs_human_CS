2489a35e9fba7324a52ec30f09f170272cf6af3e
Elise Gonthier <arkeoptys@gmail.com>
2025-11-24 10:24:26 +0000

    creation category_2

--- models/mart/mart_trad_company_seg.sql
+++ models/mart/mart_trad_company_seg.sql
@@ -2,6 +2,20 @@
     unique_id,
     channel_name,
     category,
+    CASE
+        WHEN category = 'App/website'       THEN 'Technical Support'
+        WHEN category = 'Product Queries'   THEN 'Product Information'
+        WHEN category = 'Order Related'     THEN 'Order Related'
+        WHEN category = 'Refund Related'    THEN 'Refund Related'
+        WHEN category = 'Returns'           THEN 'Returns'
+        WHEN category = 'Cancellation'      THEN 'Technical Support'
+        WHEN category = 'Feedback'          THEN 'Product Information'
+        WHEN category = 'Offers & Cashback' THEN 'Billing Question'
+        WHEN category = 'Shopzilla Related' THEN 'Technical Support'
+        WHEN category = 'Onboarding related' THEN 'Account Update'
+        WHEN category = 'Payments related' THEN 'Billing Question'
+        ELSE 'Others'
+    END AS category_2,
     sub_category,
     order_id,
     order_date,
@@ -17,13 +31,3 @@
     tenure_segment,
     csat
 FROM {{ ref('in_trad_company_seg') }}
-
-
-
-
-
-
-
-
-
-    
